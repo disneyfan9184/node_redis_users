2c2afd5562ff2a59f4e94886405dc8787f57b110
Jerry Doran <Jerry.Doran@pccairfoils.com>
2020-01-14 13:12:21 -0500

    Added redis cache

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const redis = require('redis');
+const fetch = require('node-fetch');
 
 // Create Redis client
 let client = redis.createClient();
@@ -25,6 +26,51 @@ app.use(methodOverride('_method'));
 app.get('/', (req, res, next) => {
   res.render('searchUsers');
 });
+
+function setResponse(username, repos) {
+  return `<h2>${username} has ${repos} github repos`;
+}
+
+// Make request to github for data
+async function getRepos(req, res, next) {
+  try {
+    console.log('Fetching Data...');
+
+    const { username } = req.params;
+
+    const response = await fetch(`https://api.github.com/users/${username}`);
+
+    const data = await response.json();
+
+    const repos = data.public_repos;
+
+    // Set data to redis
+    client.setex(username, 3600, repos);
+
+    res.send(setResponse(username, repos));
+  } catch (error) {
+    console.log(error);
+    res.status(500);
+  }
+}
+
+// Cache middleware
+function cache(req, res, next) {
+  const { username } = req.params;
+
+  client.get(username, (err, data) => {
+    if (err) {
+      throw err;
+    }
+    if (data !== null) {
+      res.send(setResponse(username, data));
+    } else {
+      next();
+    }
+  });
+}
+
+app.get('/repos/:username', cache, getRepos);
 
 // Search Processing
 app.post('/user/search', (req, res, next) => {
